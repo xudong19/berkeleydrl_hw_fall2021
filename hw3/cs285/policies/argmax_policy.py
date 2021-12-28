b52a4e02082501bfd7ceeac0e06ba4ed630544cc
xudong19 <dong.xu.2019@gmail.com>
2021-12-27 18:04:29 -0800

    Implement the first part: dqn. Verified with the experiments recommened in the hw3.pdf

--- hw3/cs285/policies/argmax_policy.py
+++ hw3/cs285/policies/argmax_policy.py
@@ -11,7 +11,12 @@ class ArgMaxPolicy(object):
             observation = obs
         else:
             observation = obs[None]
-        
+        n_batch = observation.shape[0]
         ## TODO return the action that maxinmizes the Q-value 
         # at the current observation as the output
+        qa_values = self.critic.qa_values(observation)
+        # assume qa_values.shape: n_batch * n_action
+        assert len(qa_values.shape) == 2, f"{qa_values.shape}"
+        assert qa_values.shape[0] == n_batch, f"{qa_values.shape}"
+        action = np.argmax(qa_values, axis=1)
         return action.squeeze()
